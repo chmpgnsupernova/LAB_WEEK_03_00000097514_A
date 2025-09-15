c2f726837d67bcdb1e74f2f442d16ab5733f9a8a
chmpgnsupernova <brianmariarvin@gmail.com>
2025-09-16 01:16:22 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03_00000097514/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03_00000097514/ListFragment.kt
@@ -1,34 +1,13 @@
 package com.example.lab_week_03_00000097514
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
-class ListFragment : Fragment(), View.OnClickListener {
-
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var coffeeListener: CoffeeListener
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is CoffeeListener) {
-            coffeeListener = context
-        } else {
-            throw RuntimeException("Must implement CoffeeListener")
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,34 +19,26 @@ class ListFragment : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val coffeeList = listOf<View>(
-            view.findViewById(R.id.affogato),
-            view.findViewById(R.id.americano),
-            view.findViewById(R.id.latte)
+        val coffeeList = listOf(
+            view.findViewById<View>(R.id.affogato),
+            view.findViewById<View>(R.id.americano),
+            view.findViewById<View>(R.id.latte)
         )
 
-        coffeeList.forEach {
-            it.setOnClickListener(this)
-        }
-    }
+        coffeeList.forEach { coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
 
-    override fun onClick(v: View?) {
-        v?.let { coffee ->
-            coffeeListener.onSelected(coffee.id)
+            coffee.setOnClickListener {
+                coffee.findNavController().navigate(
+                    R.id.coffee_id_action,
+                    fragmentBundle
+                )
+            }
         }
     }
 
     companion object {
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val COFFEE_ID = "COFFEE_ID"
     }
 }
